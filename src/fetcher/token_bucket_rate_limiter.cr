c633b31245f2dc323ebc4bd707b0497ba4d69d8a
kritoke <3081486+kritoke@users.noreply.github.com>
2026-04-15 04:52:36 -0500

    Make TokenBucketRateLimiter owner-based (actor) and introduce RateLimiterStore facade

--- src/fetcher/token_bucket_rate_limiter.cr
+++ src/fetcher/token_bucket_rate_limiter.cr
@@ -3,61 +3,92 @@ require "mutex"
 
 module Fetcher
   class TokenBucketRateLimiter
-    @tokens : Float64
-    @capacity : Float64
-    @refill_rate : Float64
-    @last_refill : Time
-    @mutex : Mutex
+    # Messages for the owner fiber
+    record TryAcquireMsg, tokens_requested : Float64, reply : Channel(Bool)
+    record AcquireMsg, tokens_requested : Float64, reply : Channel(Nil)
+    record AvailableTokensMsg, reply : Channel(Float64)
+    record TickMsg
 
     MAX_WAIT_TIME = 0.1
 
-    def initialize(capacity : Float64, refill_rate : Float64)
-      @capacity = capacity
-      @refill_rate = refill_rate
-      @tokens = capacity
+    def initialize(@capacity : Float64, @refill_rate : Float64)
+      # Owner channel and initial state
+      @cmd = Channel(TryAcquireMsg | AcquireMsg | AvailableTokensMsg | TickMsg).new
+      @tokens = @capacity
       @last_refill = Time.utc
-      @mutex = Mutex.new
+
+      # Waiter queue: Array of Tuple(tokens_requested, reply_channel)
+      @waiters = [] of Tuple(Float64, Channel(Nil))
+
+      # Spawn owner fiber
+      spawn do
+        # periodic tick to process waiters and refill tokens
+        spawn do
+          loop do
+            ::sleep(0.05.seconds)
+            @cmd.send(TickMsg.new)
+          end
+        end
+
+        loop do
+          msg = @cmd.receive
+          case msg
+          when TryAcquireMsg
+            refill_tokens
+            if @tokens >= msg.tokens_requested
+              @tokens -= msg.tokens_requested
+              msg.reply.send(true)
+            else
+              msg.reply.send(false)
+            end
+
+          when AcquireMsg
+            refill_tokens
+            if @tokens >= msg.tokens_requested
+              @tokens -= msg.tokens_requested
+              msg.reply.send(nil)
+            else
+              @waiters << {msg.tokens_requested, msg.reply}
+            end
+
+          when AvailableTokensMsg
+            refill_tokens
+            msg.reply.send(@tokens)
+
+          when TickMsg
+            refill_tokens
+            # satisfy waiters in FIFO order
+            while @waiters.size > 0 && @tokens > 0
+              req, ch = @waiters.first
+              if @tokens >= req
+                @tokens -= req
+                @waiters.shift
+                ch.send(nil)
+              else
+                break
+              end
+            end
+          end
+        end
+      end
     end
 
     def acquire(tokens : Float64 = 1.0)
-      loop do
-        wait_time = calculate_wait_time(tokens)
-        return if wait_time.nil?
-
-        sleep_time = {wait_time, MAX_WAIT_TIME}.min
-        ::sleep(sleep_time.seconds)
-      end
+      ch = Channel(Nil).new
+      @cmd.send(AcquireMsg.new(tokens, ch))
+      ch.receive
     end
 
     def try_acquire(tokens : Float64 = 1.0) : Bool
-      @mutex.synchronize do
-        refill_tokens
-        if @tokens >= tokens
-          @tokens -= tokens
-          return true
-        end
-        false
-      end
+      ch = Channel(Bool).new
+      @cmd.send(TryAcquireMsg.new(tokens, ch))
+      ch.receive
     end
 
     def available_tokens : Float64
-      @mutex.synchronize do
-        refill_tokens
-        @tokens
-      end
-    end
-
-    private def calculate_wait_time(tokens : Float64) : Float64?
-      @mutex.synchronize do
-        refill_tokens
-        if @tokens >= tokens
-          @tokens -= tokens
-          return
-        end
-        tokens_needed = tokens - @tokens
-        wait = tokens_needed / @refill_rate
-        wait < 0.001 ? 0.001 : wait
-      end
+      ch = Channel(Float64).new
+      @cmd.send(AvailableTokensMsg.new(ch))
+      ch.receive
     end
 
     private def refill_tokens
