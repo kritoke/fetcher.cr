d4d4224f458df8975916bb45941bdbc28ff36a77
kritoke <3081486+kritoke@users.noreply.github.com>
2026-05-27 20:03:02 -0500

    fix: update redirect extract spec to work with current crest version

--- spec/crest_http_client_extract_redirect_spec.cr
+++ spec/crest_http_client_extract_redirect_spec.cr
@@ -2,20 +2,6 @@ require "spec"
 require "./support/crest_http_client_test_helpers"
 require "../src/fetcher"
 
-# Minimal fake Crest::Response to simulate headers
-module Crest
-  class Response
-    getter headers : Hash(String, String | Array(String))
-    def initialize(headers)
-      @headers = headers
-      @http_client_res = nil
-      @request = nil
-      @status_code = 0
-      @body = ""
-    end
-  end
-end
-
 describe Fetcher::CrestHttpClient do
   it "extracts first Location when header is an array" do
     client = Fetcher::CrestHttpClient.new
@@ -26,9 +12,13 @@ describe Fetcher::CrestHttpClient do
   it "raises when Location header missing or empty" do
     client = Fetcher::CrestHttpClient.new
     headers = {} of String => String | Array(String)
-    ->{ client.debug_extract_redirect_url(headers) }.should raise_error(Fetcher::MissingLocationHeaderError)
+    expect_raises(Fetcher::MissingLocationHeaderError) do
+      client.debug_extract_redirect_url(headers)
+    end
 
     headers2 = {"location" => ["   "]} of String => String | Array(String)
-    ->{ client.debug_extract_redirect_url(headers2) }.should raise_error(Fetcher::MissingLocationHeaderError)
+    expect_raises(Fetcher::MissingLocationHeaderError) do
+      client.debug_extract_redirect_url(headers2)
+    end
   end
 end
